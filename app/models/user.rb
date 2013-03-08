943f856c5beaf8e3739b1bbb86d7515411c2bcd0
Dimitri Korsch <korschdima@yahoo.de>
2013-03-08 16:55:28 +0100

    some small changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,16 @@ require "maglev_record"
 
 puts "====> Loading User"
 
-class User
-  include MaglevRecord::RootedBase
+Maglev.persistent do
+  class User
+    include MaglevRecord::RootedBase
 
-  # attr_accessible :first_name, :last_name, :email, :password
-  attr_accessor :first_name, :last_name, :email, :password
+    # attr_accessible :first_name, :last_name, :email, :password
+    attr_accessor :first_name, :last_name, :email, :password
 
-  # validates :first_name, :presence => true
-  # validates :last_name, :presence => true
-  # validates :email, :presence => true
-  # validates :password, :presence => true
+    # validates :first_name, :presence => true
+    # validates :last_name, :presence => true
+    # validates :email, :presence => true
+    # validates :password, :presence => true
+  end
 end
