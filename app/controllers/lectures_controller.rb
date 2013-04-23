5a6645cb24261bfa59764ff03cd849ddc4a7d18d
Dimitri Korsch <korschdima@yahoo.de>
2013-04-23 16:50:59 +0200

    modified lectures controller for validations

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -2,6 +2,7 @@
 class LecturesController < ApplicationController
   
   def index
+    MaglevRecord.reset
     @lectures = Lecture.all
   end
 
@@ -10,24 +11,19 @@ class LecturesController < ApplicationController
   end
 
   def edit
-    # first get possible errors
-    @errors = flash[:errors]
-
-    # then reset, to get the newest content
     MaglevRecord.reset
     @lecture = Lecture.find_by_objectid(params[:id])
   end
 
   def update
     MaglevRecord.reset
-    obj = Lecture.find_by_objectid(params[:id]).update_attributes(params[:lecture])
+    @lecture = Lecture.find_by_objectid(params[:id]).update_attributes(params[:lecture])
 
-    if obj.valid?
+    if @lecture.valid?
       MaglevRecord.save
       redirect_to :action => :index
     else
-      flash[:errors] = obj.errors.full_messages
-      redirect_to :action => :edit
+      render "edit"
     end
   end
 
@@ -37,21 +33,22 @@ class LecturesController < ApplicationController
 
   def new
     @lecture = Lecture.new
-
-    # respond_to do |format|
-    #   format.html
-    #   format.json { render json: @lecture }
-    # end
   end
 
   def create
+
     MaglevRecord.reset
 
     @lecture = Lecture.new(params[:lecture]) #
 
-    MaglevRecord.save
+    if @lecture.valid?
+      MaglevRecord.save
+      redirect_to :action => :index
+    else
+      render "new"
+    end
 
-    redirect_to :action => :index
+
   end
 
 end
