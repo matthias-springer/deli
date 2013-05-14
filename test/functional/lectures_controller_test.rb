06d9626566fad582f18d17e050eafb0129fb2507
Stefan Bunk <stefan.bunk@googlemail.com>
2013-05-14 11:46:54 +0200

    Wrote tests for Lectures#edit and #update.

--- test/functional/lectures_controller_test.rb
+++ test/functional/lectures_controller_test.rb
@@ -4,7 +4,18 @@ require "functional/controller_test_base"
 class LecturesControllerTest < ActionController::TestCase
   include DeliControllerTestCase
 
-  def lecture_attrs
+  def setup
+    login_student
+    Lecture.new(lecture_params)
+    MaglevRecord.save
+  end
+  def teardown
+    User.clear
+    Lecture.clear
+    MaglevRecord.save
+  end
+
+  def lecture_params
     { :title => 'A lecture', :description => 'An interesting lecture.'}
   end
 
@@ -22,8 +33,7 @@ class LecturesControllerTest < ActionController::TestCase
   end
 
   test "should show lecture" do
-    l = Lecture.new(lecture_attrs)
-    MaglevRecord.save
+    l = Lecture.first
     get :show, :id => l.id
     assert_response :success
     assert_not_nil assigns(:lecture)
@@ -33,10 +43,24 @@ class LecturesControllerTest < ActionController::TestCase
   test "should create lecture" do
     login_admin
     prev_count = Lecture.count
-    post :create, :lecture => lecture_attrs
+    post :create, :lecture => lecture_params
     assert_equal Lecture.count, prev_count + 1
-    # assert_redirected_to lecture_path(assigns(:lecture))
-    # assert_equal 'Post was successfully created.', flash[:notice]
+    assert_redirected_to lectures_path
   end
 
+  test "should get edit" do
+    login_admin
+    get :edit, :id => Lecture.first.id
+    assert_response :success
+    assert_not_nil assigns(:lecture)
+  end
+
+  test "should update lecture" do
+    login_admin
+    params = lecture_params
+    params[:title] = "Another lecture"
+    assert_equal, Lecture.first.title = "A lecture"
+    put :update, :id => Lecture.first.id, :lecture => params
+    assert_equal, Lecture.first.title = "Another lecture"
+  end
 end
