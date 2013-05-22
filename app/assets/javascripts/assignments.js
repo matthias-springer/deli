97cc9b1870c46132e696d09f4a8c685987eaa316
Stefan Bunk <stefan.bunk@googlemail.com>
2013-05-22 09:35:00 +0200

    Added javascript for adding new assignment parts.

--- app/assets/javascripts/assignments.js
+++ app/assets/javascripts/assignments.js
@@ -1,5 +1,23 @@
 $(function() {
-  $("#add_assignment_part").on("click", function() {
-    alert("TSET");
+  $(document).on("click", ".add_assignment_part", function(e) {
+    var lastLine = $("form tr:last")
+    var nextNumber = findNextNumber(lastLine)
+    var clone = lastLine.clone()
+    clone.find("input").val("")
+    findNumberElement(clone).html("#" + nextNumber)
+    clone.appendTo(lastLine.parent())
+    $(this).remove()
   });
+
+  var findNextNumber = function(el) {
+    var numberElement = findNumberElement(el)
+    // use substr(1) to omit the leading '#'
+    var elementText = $.trim(numberElement.html()).substr(1)
+    var currentNumber = parseInt(elementText)
+    return currentNumber + 1
+  }
+
+  var findNumberElement = function(el) {
+    return el.find(".span1:first")
+  }
 });
